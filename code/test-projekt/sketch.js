82d97bc4831e868dad402ba0f7b88a87c93eb7e1
Marc Grönke <marc.groenke@web.de>
2021-11-01 19:06:50 +0100

    Erster Test

---
+++ code/test-projekt/sketch.js
@@ -0,0 +1,83 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("#3275a8");
+  fill("green")
+  stroke("green")
+  strokeWeight(0)
+  rect(0, 0, 100, 100);
+  fill("#ef42f5")
+  circle(50, 50, 85, 85);
+  fill("#29e3c7")
+  rect(0, 100, 100, 100);
+  fill("#3cbf39")
+  circle(50, 150, 85, 85);
+  fill("#9c27e6")
+  rect(0, 200, 100, 100);
+  fill("#d12e2e")
+  circle(50, 250, 85, 85);
+  fill("#0e1ba1")
+  rect(0, 300, 100, 100);
+  fill("#ffe603")
+  circle(50, 350, 85, 85);
+
+  fill("#00ccc9")
+  stroke("green")
+  strokeWeight(0)
+  rect(100, 0, 100, 100);
+  fill("red")
+  circle(150, 50, 85, 85);
+  fill("yellow")
+  rect(100, 100, 100, 100);
+  fill("blue")
+  circle(150, 150, 85, 85);
+  fill("#73ff7c")
+  rect(100, 200, 100, 100);
+  fill("#f70ad4")
+  circle(150, 250, 85, 85);
+  fill("yellow")
+  rect(100, 300, 100, 100);
+  fill("green")
+  circle(150, 350, 85, 85);
+
+  fill("#1453f5")
+  stroke("#93d93f")
+  strokeWeight(0)
+  rect(200, 0, 100, 100);
+  fill("#f57114")
+  circle(250, 50, 85, 85);
+  fill("#53cf59")
+  rect(200, 100, 100, 100);
+  fill("#593fd9")
+  circle(250, 150, 85, 85);
+  fill("#ffef63")
+  rect(200, 200, 100, 100);
+  fill("#447d3d")
+  circle(250, 250, 85, 85);
+  fill("#7d3d65")
+  rect(200, 300, 100, 100);
+  fill("#3d7d52")
+  circle(250, 350, 85, 85);
+
+  fill("#9387ff")
+  stroke("green")
+  strokeWeight(0)
+  rect(300, 0, 100, 100);
+  fill("#44fa07")
+  circle(350, 50, 85, 85);
+  fill("#61faff")
+  rect(300, 100, 100, 100);
+  fill("#e31f09")
+  circle(350, 150, 85, 85);
+  fill("#deff4d")
+  rect(300, 200, 100, 100);
+  fill("#4112a1")
+  circle(350, 250, 85, 85);
+  fill("#c5a8ff")
+  rect(300, 300, 100, 100);
+  fill("#ff5900")
+  circle(350, 350, 85, 85);
+
+}

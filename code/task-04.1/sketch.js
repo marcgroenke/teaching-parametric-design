3ddff64096ea083511969c711213af5fc0e9a198
Marc Grönke <marc.groenke@web.de>
2021-11-24 10:38:29 +0100

    svg

--- code/task-04.1/sketch.js
+++ code/task-04.1/sketch.js
@@ -3,7 +3,7 @@ let y = 0;
 let kommando = 20;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 400, SVG);
   background(255);
 }
 
@@ -19,4 +19,5 @@ function draw() {
     x = 0;
     y = y + kommando;
   }
+  save("sketch.svg");
 }
